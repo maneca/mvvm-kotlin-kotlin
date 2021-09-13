b74e3dd011ea5fcfa0fabe71b828942214cd6f16
maneca <Nasciem1986>
2021-09-13 10:11:41 +0100

    Finish UI for Countries list

--- app/src/main/java/com/example/mvvm_kotlin_kotlin/ui/CountriesFragment.kt
+++ app/src/main/java/com/example/mvvm_kotlin_kotlin/ui/CountriesFragment.kt
@@ -5,26 +5,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import coil.ImageLoader
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.LocalImageLoader
 import coil.compose.rememberImagePainter
 import coil.decode.SvgDecoder
+import com.example.mvvm_kotlin_kotlin.R
 import com.example.mvvm_kotlin_kotlin.db.model.CountriesData
 import com.example.mvvm_kotlin_kotlin.viewmodel.CountriesViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -37,7 +50,16 @@ class CountriesFragment : Fragment() {
          return ComposeView(requireContext()).apply {
             setContent {
                 val countries = countriesViewModel.countriesList.value
-                CountriesList(countries)
+                val loading = countriesViewModel.showLoading.get()
+
+                if(loading){
+                    CircularProgressIndicator(modifier = Modifier
+                        .wrapContentSize(),
+                        color = Color(0xFF7BB661),
+                        strokeWidth = 5.dp)
+                }else{
+                    CountriesList(countries)
+                }
             }
         }
 
@@ -47,7 +69,8 @@ class CountriesFragment : Fragment() {
     fun CountriesList(countries: List<CountriesData>){
         LazyColumn(modifier = Modifier
             .fillMaxHeight()
-            .fillMaxWidth()) {
+            .fillMaxWidth()
+        ) {
             items(countries) { country ->
                 CountryItem(country = country)
             }
@@ -57,21 +80,53 @@ class CountriesFragment : Fragment() {
     @ExperimentalCoilApi
     @Composable
     fun CountryItem(country: CountriesData){
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()){
-            country.flag?.let { url ->
-                loadPicture(url = url)
+        Card(
+            modifier = Modifier
+                .background(shape = RoundedCornerShape(10.dp), color = Color.LightGray)
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(2.dp)
+                .clickable { },
+            elevation = 4.dp,
+        ){
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically){
+                country.flag?.let { url ->
+                    LoadPicture(url = url)
+                }
+                Spacer(modifier = Modifier.size(4.dp))
+                Column(modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()) {
+                    Text(text = country.name!!, fontSize = 15.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Text(
+                        buildAnnotatedString {
+                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                append(stringResource(R.string.country_capital)+ " ")
+                            }
+                            append(country.capital!!)
+                        })
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Text(
+                        buildAnnotatedString {
+                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                append(stringResource(R.string.country_currency)+ " ")
+                            }
+                            append(country.currencies!!.joinToString { it.name!! })
+                        })
+                }
             }
-            Spacer(modifier = Modifier.size(4.dp))
-            Text(text = country.name!!)
-
         }
+
     }
 
+    @ExperimentalCoilApi
     @SuppressLint("UnrememberedMutableState")
     @Composable
-    fun loadPicture(url: String){
+    fun LoadPicture(url: String){
         val imageLoader = ImageLoader.Builder(LocalContext.current)
             .componentRegistry {
                 add(SvgDecoder(LocalContext.current))
